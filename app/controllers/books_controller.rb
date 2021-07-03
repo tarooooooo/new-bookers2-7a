ee8591f5dff34e08049ee137fcc0b25c1c5f8cf1
taroooooooo <ryutaromaeda83@gmail.com>
2021-07-03 13:19:11 +0000

    book一覧画面の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,29 +1,36 @@
 class BooksController < ApplicationController
-  
+
   def new
-   
+
   end
   def create
-    
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
-  
+
   def index
-   
+   @book = Book.new
+   @books = Book.all
   end
-  
+
   def show
-  
+
   end
-  
+
   def edit
     @model_class_name = ModelClassName.find(params[:id])
-  end 
-  
+  end
+
   def update
 
   end
-  
+
   def destroy
   end
-  
+   private
+   def book_params
+     params.require(:book).permit(:title,:opinion)
+   end
 end
